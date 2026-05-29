e80e689a6343d6559c793f38cb02597f1fa0f367
Tias <timur.buchkin@mail.ru>
2026-05-29 13:58:54 +0300

    update xkbswitch to latest commit

--- pkgs/xkbswitch.nix
+++ pkgs/xkbswitch.nix
@@ -8,7 +8,7 @@ vimUtils.buildVimPlugin {
   src = fetchFromGitHub {
     owner = "Tias-dev";
     repo = "xkbswitch.nvim";
-    rev = "0122936a1dc463c9bfb988214e8684b52e0d1d88";
-    hash = "sha256-BhwSM66AGqciUIkX0pPaB7c6n5Ipqv9zajGlyvvP0So=";
+    rev = "c9270633a479042d9fb47efef3da431b08a329ef";
+    hash = "sha256-jwEEyAWG7aGW9zACJmGvIhUuMN2rhKEMKw4kMbqfmKk=";
   };
 }
