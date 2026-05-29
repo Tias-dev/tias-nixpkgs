2aa285292a38cdd3fc438b2b730e8779a1912ae9
Tias <timur.buchkin@mail.ru>
2026-05-29 13:41:09 +0300

    fix incorrect hash for xkbswitch

--- pkgs/xkbswitch.nix
+++ pkgs/xkbswitch.nix
@@ -9,6 +9,6 @@ vimUtils.buildVimPlugin {
     owner = "Tias-dev";
     repo = "xkbswitch.nvim";
     rev = "0122936a1dc463c9bfb988214e8684b52e0d1d88";
-    hash = "sha256-L7k7X5PAd+OcmeyeQKZV3NQbXnEgwf9ZvXrM1o+2Clc=";
+    hash = "sha256-BhwSM66AGqciUIkX0pPaB7c6n5Ipqv9zajGlyvvP0So=";
   };
 }
